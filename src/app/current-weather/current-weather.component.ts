8d20adf1e083d5a1d5b11205df1fbfe1d633fbc2
Paulo Melo <peimelo@gmail.com>
2021-11-16 20:17:59 -0300

    Styles with flex-layout

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -17,4 +17,11 @@ export class CurrentWeatherComponent implements OnInit {
       .getCurrentWeather('Rio de Janeiro', 'BR')
       .subscribe((data) => (this.current = data));
   }
+
+  getOrdinal(date: number) {
+    const n = new Date(date).getDate();
+    return n > 0
+      ? ['th', 'st', 'nd', 'rd'][(n > 3 && n < 21) || n % 10 > 3 ? 0 : n % 10]
+      : '';
+  }
 }
